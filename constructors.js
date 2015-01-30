f623283790734f2886ae413a8be635f1199329a9
jmt75200 <joelle@pcgamerzhawaii.com>
2015-01-30 11:04:43 -1000

    Adding Spellcaster and constructors

--- constructors.js
+++ constructors.js
@@ -77,7 +77,12 @@ DamageSpell.prototype = Object.create(Spell.prototype, {
  * @property {mana} mana
  * @property {boolean} isAlive  Default value should be `true`.
  */
-
+function Spellcaster (name, health, mana){
+   this.name = name;
+   this.health = health;
+   this.mana = mana;
+   this.isAlive = true;
+}
   /**
    * The spellcaster loses health equal to `damage`.
    * Health should never be negative.
